00f7a6027b49c1e8ce0cd1c05fb0c31fab8c55a2
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-18 07:38:49 -0700

    get method for db in test branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,6 +64,12 @@ class DBStorage:
         if obj is not None:
             self.__session.delete(obj)
 
+    def get(self, cls, id):
+        """Method to retrieve obj cls from db using id"""
+        if cls and id:
+            return self.__session.query(cls).get(id)
+        return None
+    
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
