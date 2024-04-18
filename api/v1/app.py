36c2d1938383192385550cd5ced07f0f4cbf428e
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-18 19:59:50 -0400

    task 5 & 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views  # Ensure Blueprint is fully initialized before import
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     """Close SQLAlchemy session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
 
 if __name__ == "__main__":
     import os
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from . import app_views
 from flask import jsonify
 
@@ -5,3 +15,17 @@ from flask import jsonify
 def status():
     """Return the status of the API."""
     return jsonify({"status": "OK"})
+
+""" Flask route decorator indicates function will handle HTTP GET requests to /stats endpoint """
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    """ Empty dictionary to store the counts of objects by type """
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
