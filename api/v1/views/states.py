c1f91fb985560912caec9e1706466e7e9657ae60
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-19 07:33:59 -0700

    adef create

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -42,4 +42,17 @@ def create_state():
     storage.save()
     return jsonify(nstate.to_dict()), 201
     
-    
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Update a State object."""
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify({"error": "Not found"}), 404
+    if not request.json:
+        return jsonify({"error": "Not a JSON"}), 400
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in request.json.items():
+        if key not in ignore_keys:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
