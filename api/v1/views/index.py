be7f70ffec697dd98fbc76b4dcfe57c045ede0b0
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-19 05:19:43 -0700

    changed from array to dict for index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,18 +14,13 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'])
 def status():
     """Return the status of the API."""
-    return jsonify({"status": "OK"})
-
-""" Flask route decorator indicates function will handle HTTP GET requests to /stats endpoint """
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    """ Empty dictionary to store the counts of objects by type """
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    status = {cls_key: storage.count(cls_val) for cls_key, cls_val in classes.items()}
+    return jsonify(status)
