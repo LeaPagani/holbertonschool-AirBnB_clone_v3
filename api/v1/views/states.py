4373a64205b8a96fa9ac7f248a8ece192f253770
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-19 06:07:03 -0700

    def name, variable, description

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -21,3 +21,6 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Delete a State object."""
+    
