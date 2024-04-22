608386f7134bd11130ed6d4fdf4453bae52a125f
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-21 20:05:18 -0400

    pep8 for cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,8 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 
 
-@app_views.route("/states/<state_id>/cities", methods=["GET"], strict_slashes=False)
+@app_views.route("/states/<state_id>/cities",
+                 methods=["GET"], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, state_id)
@@ -38,7 +39,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
+@app_views.route("/states/<state_id>/cities",
+                 methods=["POST"], strict_slashes=False)
 def create_city(state_id):
     """Create a new City object."""
     state = storage.get(State, state_id)
